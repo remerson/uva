e2ac293500f8d64ab11cdb6ba7a5d5cec11b7f9f
Rob Emerson <remerson@fastmail.fm>
2014-04-14 21:00:15 +0100

    Added solution 530-BinomialShowdown

---
+++ solutions/530-BinomialShowdown/530.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+
+using namespace std;
+
+
+#ifndef ONLINE_JUDGE
+#define DEBUG(X) { X; }
+#else
+#define DEBUG(X)
+#endif
+
+
+int main()
+{
+  // ALso look at "369 Combinations"
+  int n, k;
+
+  while(scanf("%d %d", &n, &k) != EOF)
+  {
+    if(n == 0 && k == 0) break;
+
+    long long r = 1;
+
+    DEBUG(printf("n = %d k = %d\n", n, k));
+
+    if(k > n - k) k = n - k; // nCk = nC(n-k)
+
+    for(long long i = 1; i <= k; ++i)
+    {
+      r *= n - i + 1;
+      r /= i;
+      DEBUG(printf("i = %lld num = %lld den = %lld r = %lld\n", i, n - i + 1, i, r));
+    }
+
+    printf("%lld\n", r);
+
+  }
+
+  return 0;
+}
