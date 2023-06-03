a8ff7763242f292027732ae265c53cdad007927f
Rob Emerson <remerson@fastmail.fm>
2023-06-03 19:27:57 +0100

    Problem setup from PDF download, helper

---
+++ tools/setup-pdf.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#set -x
+
+function usage()
+{
+    echo ""
+    echo "$(basename ${0}) <Name> <UVA PDF URL>"
+    echo ""
+    echo "Quick setup tool for a given UVA problem. Downloads"
+    echo "PDF problem and creates template solution files."
+    echo ""
+    echo ""
+    echo "e.g."
+    echo " $(basename ${0}) Score https://onlinejudge.org/external/15/p1585.pdf"
+    echo ""
+}
+
+if [ $# -ne 2 ]
+then
+    usage ${0}
+    exit 1
+fi
+
+name=${1}
+pdfurl=${2}
+
+if [[ $pdfurl != *.pdf ]]
+then
+    echo "ERROR: ${pdfurl} must end in .pdf please"
+    exit 2
+fi
+
+number=$(echo $(basename ${pdfurl}) | grep -o -E "[0-9]+")
+
+echo "Downloading problem ${number} PDF: ${pdfurl}"
+
+wget --quiet -r ${pdfurl} -O $(basename ${pdfurl})
+
+target=../${number}-${name}
+
+echo "Setting up ${TARGET}"
+
+if [ -d ${target} ]
+then
+    echo "ERROR: Directory ${target} already exists!"
+    exit 2
+fi
+
+mkdir -p ${target}
+
+mv $(basename ${pdfurl}) ${target}
+cp ./judge.sh ${target}
+cp ./competitive.cpp ${target}/solution.cpp
+touch ${target}/input1.txt
+touch ${target}/expected1.txt
+touch ${target}/notes.md
+
+cd ${target}
