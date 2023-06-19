d2d581fb12fbdf8d34092cd00a6acf8ba8169aaa
Rob Emerson <remerson@fastmail.fm>
2023-06-19 17:39:24 +0100

    Added solution 13025-BackToThePast

---
+++ solutions/13025-BackToThePast/13025.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+
+#include <algorithm>
+#include <set>
+#include <map>
+#include <queue>
+#include <iostream>
+#include <iterator>
+#include <bitset>
+
+#include <unistd.h>
+
+#ifndef ONLINE_JUDGE
+#define DEBUG(X) { X; }
+#else
+#define DEBUG(X)
+#endif
+
+//using namespace std;
+
+int main()
+{
+#ifdef ONLINE_JUDGE
+  std::ios_base::sync_with_stdio(false);
+#endif
+  printf("May 29, 2013 Wednesday\n");
+
+  return 0;
+}
---
+++ solutions/13025-BackToThePast/notes.md
@@ -0,0 +1,17 @@
+# UVa 13025 BackToThePast
+
+   * https://onlinejudge.org/external/130/13025.pdf
+
+## Categories
+
+   * Trival
+
+## Approach
+
+## Hints
+
+   * Just need to work out the day.
+
+## Optimisations
+
+   * None attempted.
